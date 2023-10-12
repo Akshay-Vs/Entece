d169a7b15851bef174cb4bdb0d9640b8d31e658d
Akshay Vs <Akshay.vs2005@gmail.com>
2023-10-12 23:55:18 +0530

    Feat(sideMargin): experimenting with animations

--- components/SideMargin/SideMargin.tsx
+++ components/SideMargin/SideMargin.tsx
@@ -1,24 +1,66 @@
+'use client';
+import { AnimatePresence, motion } from 'framer-motion';
 import React from 'react';
 import styles from './SideMargin.module.scss';
 
 export const SideMargin: React.FC = () => {
+  const containerVariants = {
+    hidden: {
+      opacity: 0,
+      x: -50,
+    },
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        duration: 0.5,
+      },
+    },
+  };
+
   return (
-    <sub className={styles.margin}>
-      <h1>#INSPIRE </h1>
-      <span className={styles.brown}>
-        <h1>#GALLERIA</h1>
-      </span>
-    </sub>
+    <AnimatePresence>
+      <motion.sub
+        className={styles.margin}
+        variants={containerVariants}
+        initial="hidden"
+        animate="visible"
+        exit="hidden"
+      >
+        <h1>#INSPIRE </h1>
+        <span className={styles.brown}>
+          <h1>#GALLERIA</h1>
+        </span>
+      </motion.sub>
+    </AnimatePresence>
   );
 };
-
 export const RightMargin: React.FC = () => {
+  const containerVariants = {
+    hidden: {
+      opacity: 0,
+      x: 50,
+    },
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        duration: 0.5,
+      },
+    },
+  };
+
   return (
-    <span className={styles.rightMargin}>
+    <motion.span
+      className={styles.rightMargin}
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+    >
       <h1>#SHOP</h1>
       <span className={styles.gray}>
         <h1>#SHOP</h1>
       </span>
-    </span>
+    </motion.span>
   );
 };
